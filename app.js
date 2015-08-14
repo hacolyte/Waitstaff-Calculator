f7587ad2494dec7b6ed97d8732aa95aa85a2e2c1
hacolyte <hacolyte0@gmail.com>
2015-08-14 19:47:56 -0400

    add customer charges totals

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ angular.module('waitstaffApp', [])
 
 ////////Controller Functions////////////
 function waitstaffController($scope){
+	$scope.subTotal = 0;
+	$scope.tip = 0;
+	$scope.total = 0;
 	$scope.submit = function(){
 		var baseMealPrice = $scope.baseMealPrice;
 		var taxRate = $scope.taxRate;
 		var tipAmount = $scope.tipAmount;
-		var total = baseMealPrice + (baseMealPrice * (taxRate / 100)) + (baseMealPrice * (tipAmount / 100));
-		console.log(total);
-		return total;
+		calculateTip(baseMealPrice, taxRate, tipAmount);
+	}
+	function calculateTip(bmp, tr, t){
+		$scope.subTotal = bmp + (bmp * (tr / 100));
+		$scope.tip = bmp * (t / 100);
+		$scope.total = bmp + (bmp * (tr / 100)) + (bmp * (t / 100));
 	}
 }
