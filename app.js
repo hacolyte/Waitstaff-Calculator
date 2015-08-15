8fc24b28d979680c3d4a514de8971c8e73add0b7
hacolyte <hacolyte0@gmail.com>
2015-08-15 12:01:58 -0400

    add my earnings info calculations

--- app.js
+++ app.js
@@ -5,17 +5,34 @@ angular.module('waitstaffApp', [])
 ////////Controller Functions////////////
 function waitstaffController($scope){
 	$scope.subTotal = 0;
-	$scope.tip = 0;
+	$scope.tipCash = 0;
 	$scope.total = 0;
+	$scope.tipTotal = 0;
+	$scope.mealCount = 0;
+	$scope.averageTip = 0;
+	var tipsArr = [];
+	var mealCount = 0;
 	$scope.submit = function(){
 		var baseMealPrice = $scope.baseMealPrice;
 		var taxRate = $scope.taxRate;
-		var tipAmount = $scope.tipAmount;
-		calculateTip(baseMealPrice, taxRate, tipAmount);
+		var tipPercentage = $scope.tipPercentage;
+		calculateTip(baseMealPrice, taxRate, tipPercentage);
+		tipTotal(baseMealPrice, tipPercentage);
+		$scope.mealCount = mealCount += 1;
 	}
 	function calculateTip(bmp, tr, t){
 		$scope.subTotal = bmp + (bmp * (tr / 100));
-		$scope.tip = bmp * (t / 100);
+		$scope.tipCash = bmp * (t / 100);
 		$scope.total = bmp + (bmp * (tr / 100)) + (bmp * (t / 100));
+	}
+	function tipTotal(bmp, t){
+		var eachTipCash = bmp * (t / 100);
+		var tipsTotal = 0;
+		tipsArr.push(eachTipCash);
+		for (var i = 0; i < tipsArr.length; i++) {
+			tipsTotal += tipsArr[i];
+		};
+		$scope.tipTotal = tipsTotal;
+		$scope.averageTip = (tipsTotal / mealCount);
 	}
 }
