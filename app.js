858f920ab5fd38638d46aa479a3683f2092560bc
hacolyte <hacolyte0@gmail.com>
2015-08-19 15:09:38 -0400

    refactor reset

--- app.js
+++ app.js
@@ -4,36 +4,37 @@ angular.module('waitstaffApp', [])
 
 ////////Controller Functions////////////
 function waitstaffController($scope){
-	$scope.subTotal = 0;
-	$scope.tipCash = 0;
-	$scope.total = 0;
-	$scope.tipTotal = 0;
-	$scope.mealCount = 0;
-	$scope.averageTip = 0;
+	$scope.data = {};
+	$scope.data.subTotal = 0;
+	$scope.data.tipCash = 0;
+	$scope.data.total = 0;
+	$scope.data.tipTotal = 0;
+	$scope.data.mealCount = 0;
+	$scope.data.averageTip = 0;
 	var tipsArr = [];
 	var mealCount = 0;
 	$scope.submit = function(){
 		if($scope.mealTotal.$valid){
 			console.log("the form is valid");
-			var baseMealPrice = $scope.baseMealPrice;
-			var taxRate = $scope.taxRate;
-			var tipPercentage = $scope.tipPercentage;
-			$scope.mealCount = mealCount += 1;
+			var baseMealPrice = $scope.data.baseMealPrice;
+			var taxRate = $scope.data.taxRate;
+			var tipPercentage = $scope.data.tipPercentage;
+			$scope.data.mealCount = mealCount += 1;
 			calculateTip(baseMealPrice, taxRate, tipPercentage);
 			tipTotal(baseMealPrice, tipPercentage);
 			// Reset the form
-			$scope.baseMealPrice = '';
-			$scope.taxRate = '';
-			$scope.tipPercentage = '';
+			$scope.data.baseMealPrice = '';
+			$scope.data.taxRate = '';
+			$scope.data.tipPercentage = '';
 			$scope.mealTotal.$setPristine();
 		}else{
 			console.log("The form is invalid");
 		}
 	}
 	function calculateTip(bmp, tr, t){
-		$scope.subTotal = bmp + (bmp * (tr / 100));
-		$scope.tipCash = bmp * (t / 100);
-		$scope.total = bmp + (bmp * (tr / 100)) + (bmp * (t / 100));
+		$scope.data.subTotal = bmp + (bmp * (tr / 100));
+		$scope.data.tipCash = bmp * (t / 100);
+		$scope.data.total = bmp + (bmp * (tr / 100)) + (bmp * (t / 100));
 	}
 	function tipTotal(bmp, t){
 		var eachTipCash = bmp * (t / 100);
@@ -42,26 +43,19 @@ function waitstaffController($scope){
 		for (var i = 0; i < tipsArr.length; i++) {
 			tipsTotal += tipsArr[i];
 		};
-		$scope.tipTotal = tipsTotal;
-		$scope.averageTip = (tipsTotal / mealCount);
+		$scope.data.tipTotal = tipsTotal;
+		$scope.data.averageTip = (tipsTotal / mealCount);
 	}
 	$scope.cancel = function(){
-		$scope.baseMealPrice = '';
-		$scope.taxRate = '';
-		$scope.tipPercentage = '';
+		$scope.data.baseMealPrice = '';
+		$scope.data.taxRate = '';
+		$scope.data.tipPercentage = '';
 		$scope.mealTotal.$setPristine();
 	}
 	$scope.reset = function(){
 		tipsArr = [];
 		mealCount = 0;
-		$scope.baseMealPrice = '';
-		$scope.taxRate = '';
-		$scope.tipPercentage = '';
-		$scope.subTotal = " ";
-		$scope.tipCash = '';
-		$scope.total = '';
-		$scope.tipTotal = '';
-		$scope.averageTip = '';
+		$scope.data = {};
 		$scope.mealTotal.$setPristine();
 		console.log(tipsArr);
 	}
